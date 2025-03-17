c7654a0ce60b8cf019288dbc0804ea9c30dce078
nauman <naumanyousaf026@gmail.com>
2025-03-16 17:30:01 -0700

    Remove contact component from contactus.jsx

--- src/component/contact/Contactus.jsx
+++ src/component/contact/Contactus.jsx
@@ -2,14 +2,12 @@ import React from "react";
 import { Form } from "react-router-dom";
 import Header from "../Header";
 import Footer from "../Footer";
-import ContactUs from "./form";
 import ContactInfo from "./ContactInfo";
 
 function Contactus() {
   return (
     <div>
       <Header />
-      <ContactUs />
       <ContactInfo />
       <Footer />
     </div>
