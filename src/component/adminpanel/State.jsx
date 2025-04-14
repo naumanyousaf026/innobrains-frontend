2e578077155ea21c9cc712876ff9f06a92e5ace3
nauman <naumanyousaf026@gmail.com>
2025-04-14 04:52:50 -0700

    my code after adding state page and stateForm

--- src/component/adminpanel/State.jsx
+++ src/component/adminpanel/State.jsx
@@ -76,7 +76,7 @@ export default function StatePreview() {
           ].map(({ label, value }) => (
             <div
               key={label}
-              className=" bg-[#103153]  rounded-xl p-6 text-center shadow-md "
+              className=" bg-[#10315308]  rounded-xl p-6 text-center shadow-md "
             >
               <h4 className="text-sm  nunito-sans font-medium uppercase tracking-wider mb-2">
                 {label}
