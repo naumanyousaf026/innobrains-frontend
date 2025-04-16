6cf8496db5f85d17525d27d063f940380e2cb138
nauman <naumanyousaf026@gmail.com>
2025-04-16 00:54:13 -0700

    my code after adding achivement

--- src/component/adminpanel/AchievementForm.jsx
+++ src/component/adminpanel/AchievementForm.jsx
@@ -53,7 +53,7 @@ function AchievementForm() {
 
   return (
     <div className="flex  items-center justify-center min-h-screen  w-full  p-6">
-      <div className="w-full max-w-3xl bg-white rounded-lg shadow-xl p-8">
+      <div className="w-full  bg-white rounded-lg shadow-xl p-8">
         <div className="text-center mb-8">
           <h2 className="text-3xl font-extrabold mb-2">Achievement</h2>
         </div>
